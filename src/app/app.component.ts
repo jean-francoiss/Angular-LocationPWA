32ea30f25e2f33b55de727669fccad2a42959a0d
jean-francoiss <dimer.hacker@gmail.com>
2022-06-28 04:48:21 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appgeo';
+
+  map_url = "";
+  latitude = 0;
+  longitude = 0;
+
+  latitude2 = 0;
+  longitude2 = 0;
+
+  ngOnInit(){
+    this.verifica_local();
+  }
+
+  verifica_local(){
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(
+        (pos)=>{
+          this.latitude = pos.coords.latitude;
+          this.longitude = pos.coords.longitude;
+          this.map_url = 'https://www.google.com.br/maps/search/?api=1&query=' + this.latitude + ',' + this.longitude;
+        },
+        ()=> {
+          alert("Sem localização");
+        },
+        {timeout: 15000}
+      );
+      navigator.geolocation.watchPosition(
+        (pos)=>{
+          this.latitude2 = pos.coords.latitude;
+          this.longitude2 = pos.coords.longitude;
+        },
+        ()=> {
+          alert("Sem rastreamento");
+        },
+        {timeout: 15000}
+      );
+    }else {
+      alert("Sem permissão")
+    }
+  }
 }
